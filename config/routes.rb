4b9cbe7c758676939f7d561f92a3301d5d8a92f8
Tom Arra <tarra3@gmail.com>
2011-04-10 08:29:17 -0500

    Adding ResetCSS from YUI. Will most likely break the views right now but will make cross browser support better.

--- config/routes.rb
+++ config/routes.rb
@@ -1,77 +1,75 @@
 Lunch::Application.routes.draw do
-  scope '(:locale)' do
-    resources :application
-    match '/' => 'application#index'
+  resources :application
+  match '/' => 'application#index'
 
-    resources :users
-    match '/register' => 'users#new'
-    match '/login' => 'users#login', :as => 'login'
-    match '/logout' => 'users#logout', :as => 'logout'
+  resources :users
+  match '/register' => 'users#new'
+  match '/login' => 'users#login', :as => 'login'
+  match '/logout' => 'users#logout', :as => 'logout'
 
 
-    match 'selections' => 'selections#index'
-    match 'selections/current' => 'selections#current'
-    match 'selections/vote/:selection_id/:vote_value' => 'selections#vote', :as => 'vote'
+  match 'selections' => 'selections#index'
+  match 'selections/current' => 'selections#current'
+  match 'selections/vote/:selection_id/:vote_value' => 'selections#vote', :as => 'vote'
 
-    resources :categories
+  resources :categories
 
-    resources :places
+  resources :places
 
-    # The priority is based upon order of creation:
-    # first created -> highest priority.
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
-    # Sample of regular route:
-    #   match 'products/:id' => 'catalog#view'
-    # Keep in mind you can assign values other than :controller and :action
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
 
-    # Sample of named route:
-    #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-    # This route can be invoked with purchase_url(:id => product.id)
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
 
-    # Sample resource route (maps HTTP verbs to controller actions automatically):
-    #   resources :products
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
 
-    # Sample resource route with options:
-    #   resources :products do
-    #     member do
-    #       get 'short'
-    #       post 'toggle'
-    #     end
-    #
-    #     collection do
-    #       get 'sold'
-    #     end
-    #   end
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
 
-    # Sample resource route with sub-resources:
-    #   resources :products do
-    #     resources :comments, :sales
-    #     resource :seller
-    #   end
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
 
-    # Sample resource route with more complex sub-resources
-    #   resources :products do
-    #     resources :comments
-    #     resources :sales do
-    #       get 'recent', :on => :collection
-    #     end
-    #   end
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
 
-    # Sample resource route within a namespace:
-    #   namespace :admin do
-    #     # Directs /admin/products/* to Admin::ProductsController
-    #     # (app/controllers/admin/products_controller.rb)
-    #     resources :products
-    #   end
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
 
-    # You can have the root of your site routed with "root"
-    # just remember to delete public/index.html.
-    # root :to => "welcome#index"
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
-    # See how all your routes lay out with "rake routes"
+  # See how all your routes lay out with "rake routes"
 
-    # This is a legacy wild controller route that's not recommended for RESTful applications.
-    # Note: This route will make all actions in every controller accessible via GET requests.
-    # match ':controller(/:action(/:id(.:format)))'
-  end
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
 end
