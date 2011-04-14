e34562265f944a27957e9f3ca63e9a6061d0c71a
Tom Arra <tarra3@gmail.com>
2011-04-13 21:58:13 -0500

    Adding in some theming. This is just a starting point. Don't know that this fully takes care of #16 right now but it is a step in the right direction. I carried over all the links we had in the layout and made sure they were all working as well.

--- rails/config/environment.rb
+++ rails/config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Lunch::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  if html_tag =~ /<label/
+    %|<div class="fieldWithErrors">#{html_tag} <span class="error">#{[instance.error_message].join(', ')}</span></div>|
+  else
+    html_tag
+  end
+end
